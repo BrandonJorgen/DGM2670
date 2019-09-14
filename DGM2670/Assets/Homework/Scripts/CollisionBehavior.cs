ad2c348dc9459c872c7a0a8ba915bc6e28a2dd6e
Brandon <brandonjorgen@gmail.com>
2019-09-14 10:07:15 -0600

    UnityAction'd ScoreScript

---
+++ DGM2670/Assets/Homework/Scripts/CollisionBehavior.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class CollisionBehavior : MonoBehaviour
+{
+    public UnityEvent OnCollisionEvent;
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        OnCollisionEvent.Invoke();
+    }
+}
