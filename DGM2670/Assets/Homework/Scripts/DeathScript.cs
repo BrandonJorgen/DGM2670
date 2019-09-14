193c47ca2a6e1ff3e9f708487037fae741d6ca01
Brandon <brandonjorgen@gmail.com>
2019-09-14 13:29:22 -0600

    Unity Action'd Game Logic
    Split script into 4 scripts

---
+++ DGM2670/Assets/Homework/Scripts/DeathScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class DeathScript : MonoBehaviour
+{
+    public GameAction action;
+    public FloatData healthData;
+    public UnityEvent deathEvent;
+
+    private void Start()
+    {
+        action.action += DeathCheck;
+    }
+
+    private void DeathCheck()
+    {
+        if (healthData.value <= 0)
+        {
+            deathEvent?.Invoke();
+        }
+    }
+}
--- DGM2670/Assets/Homework/Scripts/GameLogic.cs
+++ DGM2670/Assets/Homework/Scripts/GameLogic.cs
@@ -4,88 +4,32 @@ using UnityEngine.SceneManagement;
 
 public class GameLogic : MonoBehaviour
 {
-    [Header("Victory Settings")]
-    public GameObject Boss;
-    public UnityEvent BossKilled;
-    private bool bossSighted =false;
-    [Header("Pause Game Settings")] 
-    public BoolData Paused;
-    public GameObject QuitMenu;
-    public UnityEvent OnPaused;
-    public UnityEvent OnResume;
-    [Header("Game Over Settings")]
-    public FloatData PlayerHealthData;
-    public UnityEvent ZeroHealth;
-    private GameObject player;
-
-    private void OnEnable()
-    {
-        SceneManager.sceneLoaded += SceneChangeManagement;
-    }
+    public UnityEvent sceneChange;
     
-    private void OnDisable()
-    {
-        SceneManager.sceneLoaded -= SceneChangeManagement;
-    }
+    [Header("Pause Game Settings")]
+    public UnityEvent EscapePressedEvent;
+
+    private void OnEnable() 
+    { 
+        SceneManager.sceneLoaded += SceneChangeManagement; 
+    } 
+     
+    private void OnDisable() 
+    { 
+        SceneManager.sceneLoaded -= SceneChangeManagement; 
+    } 
+     
+    private void SceneChangeManagement(Scene scene, LoadSceneMode current) 
+    { 
+        sceneChange.Invoke();
+    } 
     
-    private void SceneChangeManagement(Scene scene, LoadSceneMode current)
-    {
-        player = GameObject.FindWithTag("Player");
-        Boss = GameObject.FindWithTag("Boss");
-
-        //Check if a boss even exists in the current Stage
-        if (Boss != null)
-        {
-            bossSighted = true;
-        }
-        else
-        {
-            bossSighted = false;
-        }
-    }
-
     void Update()
     {
-        //VICTORY BEHAVIOR
-        if (bossSighted)//If Boss exists
-        {
-            if (Boss.activeSelf == false)//If boss is dead
-            {
-                BossKilled.Invoke();
-            }
-        }
-        
         //PAUSE GAME BEHAVIOR
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (Time.timeScale >= 1)
-            {
-                Time.timeScale = 0; //game set to paused
-                Paused.Bool = true;
-                OnPaused.Invoke();
-            } 
-            else if (Time.timeScale <= 0)
-            {
-                if (!QuitMenu.activeSelf)//The Quit Menu isn't active
-                {
-                    Time.timeScale = 1; //game set back to playing
-                    Paused.Bool = false;
-                    OnResume.Invoke();
-                }
-
-                if (QuitMenu.activeSelf)//The Quit Menu is active
-                {
-                    QuitMenu.SetActive(false);
-                }
-            }
-        }
-        
-        //GAME OVER BEHAVIOR
-        // Check if Player Health is at least 0
-        if (PlayerHealthData.Value <= 0) //Zero Health
-        {
-            ZeroHealth.Invoke();
-            player.GetComponent<CharacterController>().enabled = false;
+            EscapePressedEvent?.Invoke();
         }
     }
 }
