bbdf710bdab342723b2f7936890fbe25788567d0
Brandon <brandonjorgen@gmail.com>
2019-09-22 13:59:28 -0600

    Added Tower Defense File Structure/Added Main Menu Logic

---
+++ DGM2670/Assets/Tower Defense/Scripts/MainMenuLogic.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuLogic : MonoBehaviour
+{
+    public void LoadLevel(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game has been quit");
+    }
+    
+    public void QualityChange(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+        Debug.Log(qualityIndex);
+    }
+}
