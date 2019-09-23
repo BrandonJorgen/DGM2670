0ac498453495ae5808b5980b525077eaa11ed898
Brandon <brandonjorgen@gmail.com>
2019-09-23 14:58:51 -0600

    9/23 Lecture

---
+++ DGM2670/Assets/Anthony Stuff/Scripts/CharacterMover.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMover : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 position;
+    private int jumpCount;
+    public float moveSpeed = 10f, jumpSpeed = 30f, gravity = 3f;
+    public int jumpCountMax = 2;
+    
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        position.x = moveSpeed * Input.GetAxis("Horizontal");
+        position.z = moveSpeed * Input.GetAxis("Vertical");
+        position.y -= gravity;
+
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
+        {
+            position.y = jumpSpeed;
+            jumpCount++;
+        }
+        
+        if (controller.isGrounded)
+        {
+            position.y = 0;
+            jumpCount = 0;
+        }
+        
+        controller.Move(position * Time.deltaTime);
+        
+    }
+}
