8ca834cafef8d7fb1eb673f9ee03c9db3cf7021e
Brandon <brandonjorgen@gmail.com>
2019-09-26 13:49:55 -0600

    Added Enemy Movement Script/Updated Game Scene

---
+++ DGM2670/Assets/Tower Defense/Scripts/EnemyMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyMovement : MonoBehaviour
+{
+    private NavMeshAgent navAgent;
+    public FloatData movementSpeed;
+    public Transform destination;
+    
+    void Start()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+        navAgent.speed = movementSpeed.value;
+        navAgent.destination = destination.position;
+    }
+}
---
+++ DGM2670/Assets/Tower Defense/Scripts/ObjectInstantiate.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectInstantiate : MonoBehaviour
+{
+    //# of objects to instantiate
+    //What to instantiate
+    //Where to instantiate
+}
