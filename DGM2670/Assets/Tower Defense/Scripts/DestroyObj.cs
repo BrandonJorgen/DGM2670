718b33f286f196059dd808738aa4dd848b6b7f62
Brandon <brandonjorgen@gmail.com>
2019-09-27 10:41:08 -0600

    Updated Enemy Prefab

--- DGM2670/Assets/Tower Defense/Scripts/DestroyObj.cs
+++ DGM2670/Assets/Tower Defense/Scripts/DestroyObj.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DestroyObj : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision other)
+    private void OnTriggerEnter(Collider other)
     {
         Destroy(other.gameObject);
     }
