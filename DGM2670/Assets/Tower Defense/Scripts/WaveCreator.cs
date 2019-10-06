3149dd09f7435dd34add9527e1912380d255298a
Brandon <brandonjorgen@gmail.com>
2019-10-05 19:50:30 -0600

    Started Spawning System

---
+++ DGM2670/Assets/Tower Defense/Scripts/WaveCreator.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class WaveCreator : ScriptableObject
+{
+    public GameObject enemyPrefab;
+    public int amountToSpawn = 1;
+}
---
+++ DGM2670/Assets/Tower Defense/Scripts/WaveSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    //Every new wave: Spawn Set
+    //Use Wave Creator
+
+    public List<WaveCreator> waves;
+    public FloatData currentWave;
+    private Transform spawnPoint;
+
+    private void Start()
+    {
+        spawnPoint = GetComponent<Transform>();
+    }
+
+    public void UpdateWave()
+    {
+        currentWave.value++;
+        
+        for (int i = 0; i < currentWave.value; i++)
+        {
+            SpawnWave(i - 1);
+        }
+    }
+
+    public void SpawnWave(int waveToSpawn)
+    {
+        Instantiate(waves[waveToSpawn].enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
