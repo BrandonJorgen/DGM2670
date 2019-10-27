5e96f155c09e408b12d0f62c7eebf1a97d507dad
Brandon <brandonjorgen@gmail.com>
2019-10-27 17:24:54 -0600

    Player Firing fixed/Added Player Death settings

--- DGM2670/Assets/Tank Game/Scripts/LimitedInstatiateObj.cs
+++ DGM2670/Assets/Tank Game/Scripts/LimitedInstatiateObj.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 public class LimitedInstatiateObj : MonoBehaviour
 {
     public GameObject prefabToInstatiate;
-    public List<GameObject> instObjs;
+    public FloatData instObjCount;
     
     public void Spawn()
     {
-        if (instObjs.Count == 0)
+        if (instObjCount.value <= 0)
         {
-            var instObj = Instantiate(prefabToInstatiate, transform.position, Quaternion.identity);
-            instObjs.Add(instObj);
+            Instantiate(prefabToInstatiate, transform.position, transform.rotation);
+            instObjCount.value++;
         }
     }
 }
---
+++ DGM2670/Assets/Tank Game/Scripts/OnDestroyBehavior.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class OnDestroyBehavior : MonoBehaviour
+{
+    public UnityEvent onDestroyEvent;
+
+    private void OnDestroy()
+    {
+        onDestroyEvent?.Invoke();
+    }
+}
