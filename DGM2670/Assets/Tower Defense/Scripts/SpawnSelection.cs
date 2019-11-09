294080845a9f8e6b3f4d506b0794e2822ff35acf
Brandon <brandonjorgen@gmail.com>
2019-11-09 16:59:32 -0700

    Starting some Spawn Stuff

---
+++ DGM2670/Assets/Tower Defense/Scripts/SpawnSelection.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class SpawnSelection : MonoBehaviour
+{
+    public UnityEvent locationOne, locationTwo;
+    public float result;
+
+    public void GetNumber()
+    {
+        result = Random.Range(1, 3);
+    }
+
+    public void SelectSpawn()
+    {
+        GetNumber();
+
+        if (result == 1f)
+        {
+            locationOne?.Invoke();
+        }
+        else
+        {
+            locationTwo?.Invoke();
+        }
+    }
+}
